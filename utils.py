baf1ba0331b89ca7c0229989426cac3fa82e8cbc
Krystaszek Kacper <kacper.krystaszek@nokia.com>
2024-05-28 17:36:54 +0200

    initial

---
+++ utils.py
@@ -0,0 +1,55 @@
+"""
+Test module for CI/CD
+"""
+
+
+def add(a: int, b: int) -> int:
+    """adds two numbers
+
+    Args:
+        a (int): number a
+        b (int): number b
+
+    Returns:
+        int: result
+    """
+    return a + b
+
+
+def subtract(a: int, b: int) -> int:
+    """subtracts two numbers
+
+    Args:
+        a (int): number a
+        b (int): number b
+
+    Returns:
+        int: result
+    """
+    return a - b
+
+
+def multiply(a: int, b: int) -> int:
+    """multiplies two numbers
+
+    Args:
+        a (int): number a
+        b (int): number b
+
+    Returns:
+        int: result
+    """
+    return a * b
+
+
+def divide(a: int, b: int) -> float:
+    """divides two numbers
+
+    Args:
+        a (int): number a
+        b (int): number b
+
+    Returns:
+        int: result
+    """
+    return a / b
